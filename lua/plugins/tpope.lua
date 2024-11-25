6d54882df2989068ab57170599b5a3cb155ce9fb
Austin Sloane <sloane.ats@gmail.com>
2024-11-25 11:16:53 -0800

    add tpope rsi plugin to add readline keybinds in enter mode and update

--- lua/plugins/tpope.lua
+++ lua/plugins/tpope.lua
@@ -1,8 +1,11 @@
 -- plugins by tpope
+-- https://github.com/tpope/vim-rsi
 -- https://github.com/tpope/vim-sleuth
 -- https://github.com/tpope/vim-unimpaired
 
 return { -- Collection of vim plugins by the pope himself
+  -- Use readline keybindings in insert mode when not at end of line
+  'tpope/vim-rsi',
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
   -- Complementary pairs of mappings
