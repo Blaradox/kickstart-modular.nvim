46220724724cbdcfd14d99031e0c6320aabca164
Austin Sloane <sloane.ats@gmail.com>
2024-11-24 18:40:35 -0800

    only set cursorline on active windows

--- plugin/autocommands.lua
+++ plugin/autocommands.lua
@@ -12,4 +12,22 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Only set cursorline on activce window/buffer
+vim.api.nvim_create_autocmd({ 'WinEnter', 'BufEnter' }, {
+  desc = 'Set cursorline when entering window/buffer',
+  group = vim.api.nvim_create_augroup('cursorline-enter', { clear = true }),
+  callback = function()
+    vim.opt.cursorline = true
+  end,
+})
+
+-- Remove cursorline when leaving window/buffer
+vim.api.nvim_create_autocmd({ 'WinLeave', 'BufLeave' }, {
+  desc = 'Remove cursorline when leaving window/buffer',
+  group = vim.api.nvim_create_augroup('cursorline-leave', { clear = true }),
+  callback = function()
+    vim.opt.cursorline = false
+  end,
+})
+
 -- vim: ts=2 sts=2 sw=2 et
