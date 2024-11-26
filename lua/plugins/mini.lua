19fdb78f3ee9f017d8a20d12c7350a95d304c398
Austin Sloane <sloane.ats@gmail.com>
2024-11-26 15:02:06 -0800

    add mini operators to config

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -7,7 +7,6 @@ return {
     config = function()
       -- Better Around/Inside textobjects
       --
-      -- Examples:
       --  - va)  - [V]isually select [A]round [)]paren
       --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
       --  - ci'  - [C]hange [I]nside [']quote
@@ -22,6 +21,17 @@ return {
 
       -- Automatically add/delete matching parens, quotes, etc.
       require('mini.pairs').setup()
+
+      -- Evaluate/Exchange/Multiply/Replace/Sort text
+      --
+      --  - g=ib - Evaluate [I]nner [B]rackets and repalce with output.
+      --  - gxiw - E[x]change [I]nner [W]ord with next exchange.
+      --  - gxx  - E[x]change current line with next exchange.
+      --  - gmaw - [M]ultiply [A]round [W]ord, i.e. add 1 copy of word (with spaces).
+      --  - 2gmm - [M]ultiply line twice, i.e. add 2 copies of line.
+      --  - griw - [R]eplace [I]nner [W]ord with default register.
+      --  - gsip - [S]ort [I]nner [P]aragraph lines.
+      require('mini.operators').setup()
 
       -- Simple and easy statusline.
       local statusline = require 'mini.statusline'
